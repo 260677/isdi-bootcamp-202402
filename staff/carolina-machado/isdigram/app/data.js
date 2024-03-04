5a107b10e869c7528c4939078f1c6e4cc1685473
Carolina Machado <carolinamachado@Carolinas-MacBook-Air.local>
2024-03-04 16:49:49 +0100

    added register.js, logic.js and more to folder #333

---
+++ staff/carolina-machado/isdigram/app/data.js
@@ -0,0 +1,13 @@
+// data
+
+var users = localStorage.users ? JSON.parse(localStorage.users) : []
+
+/*
+
+var users = []
+
+if (localStore.users) {
+    users = JSON.parse(localStorage.users);
+} else {
+    return users
+*/
